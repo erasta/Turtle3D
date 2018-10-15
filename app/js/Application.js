b7cf2e45a3cdc39a6ae8efbce4f62c3c910901f1
Eran Geva <>
2018-10-15 17:45:12 +0300

    tubes instead of lines

--- app/js/Application.js
+++ app/js/Application.js
@@ -8,7 +8,7 @@ class Application {
         this.changeByPreset();
         this.initGui();
         this.materialLine = new THREE.LineBasicMaterial({ color: 'red' });//, linewidth:1 });
-        this.materialMesh = new THREE.MeshLambertMaterial({ color: 'red' });
+        this.materialMesh = new THREE.MeshStandardMaterial({ color: 'red', side: THREE.DoubleSide });
 
         this.applyGuiChanges();
     }
@@ -19,21 +19,40 @@ class Application {
         var rules = [this.rule1, this.rule2, this.rule3, this.rule4, this.rule5];
         rules = rules.filter((r) => r && r.length > 0);
         t.rule(this.axiom, rules);
-        // if (this.width === 0) {
+        var vertices = t.go(this.iterations);
+        if (this.width === 0) {
             this.mesh = new THREE.LineSegments(new THREE.Geometry(), this.materialLine);
-            this.mesh.geometry.vertices = t.go(this.iterations);
+            this.mesh.geometry.vertices = vertices;
             // this.materialLine.linewidth = this.width;
-        // } else {
-        //     this.mesh = new THREE.Mesh(new THREE.Geometry(), this.materialMesh);
-        //     t.createSequances(this.iterations).forEach(s => {
-        //         let path = new THREE.CurvePath();
-        //         for (let i = 1; i < s.length; ++i) {
-        //             path.curves.push(new THREE.LineCurve3(s[i - 1], s[i]));
-        //         }
-        //         let tube = new THREE.TubeGeometry(path, s.length * 4, this.width, 6, true);
-        //         this.mesh.geometry.merge(tube);
-        //     });
-        // }
+        } else {
+            this.mesh = new THREE.Mesh(new THREE.Geometry(), this.materialMesh);
+            let sp = new THREE.SphereGeometry(this.width);
+            for (var i = 0; i < vertices.length; ++i) {
+                sp.translate(vertices[i].x, vertices[i].y, vertices[i].z);
+                this.mesh.geometry.merge(sp);
+                sp.translate(-vertices[i].x, -vertices[i].y, -vertices[i].z);
+            }
+            // for (var i = 0; i < vertices.length; i += 2) {
+            //     let path = new THREE.CurvePath();
+            //     path.curves.push(new THREE.LineCurve3(vertices[i], vertices[i + 1]));
+            //     let tube = new THREE.TubeGeometry(path, 10, this.width, 6, false);
+            //     this.mesh.geometry.merge(tube);
+            // }
+            for (var i = 0; i < vertices.length; i += 2) {
+                // let path = new THREE.CurvePath();
+                // path.curves.push(new THREE.LineCurve3(vertices[i], vertices[i + 1]));
+                let tube = new THREE.TubeGeometry(new THREE.LineCurve3(vertices[i], vertices[i + 1]), 2, this.width, 6, false);
+                this.mesh.geometry.merge(tube);
+            }
+            // t.createSequances(this.iterations).forEach(s => {
+            //     let path = new THREE.CurvePath();
+            //     for (let i = 1; i < s.length; ++i) {
+            //         path.curves.push(new THREE.LineCurve3(s[i - 1], s[i]));
+            //     }
+            //     let tube = new THREE.TubeGeometry(path, s.length * 4, this.width, 6, true);
+            //     this.mesh.geometry.merge(tube);
+            // });
+        }
         this.mesh.geometry.computeBoundingBox();
         this.mesh.position.set(-this.mesh.geometry.boundingBox.getCenter().x, -this.mesh.geometry.boundingBox.getCenter().y, 0);
 
@@ -58,7 +77,7 @@ class Application {
         this.gui.add(this, 'preset', this.presets.map(p => p.preset)).onChange(this.changeByPreset);
         this.gui.add(this, 'angle').step(0.001).min(0).max(360).onChange(this.applyGuiChanges);
         this.gui.add(this, 'distance').step(0.001).min(0.01).max(100).onChange(this.applyGuiChanges);
-        // this.gui.add(this, 'width').step(0.001).min(0).max(5).onChange(this.applyGuiChanges);
+        this.gui.add(this, 'width').step(0.001).min(0).max(5).onChange(this.applyGuiChanges);
         this.gui.add(this, 'iterations').step(1).min(0).max(20).onChange(this.applyGuiChanges);
         this.gui.add(this, 'axiom').onChange(this.applyGuiChanges);
         this.gui.add(this, 'rule1').onChange(this.applyGuiChanges);
@@ -70,14 +89,14 @@ class Application {
 
     createPresets() {
         this.presets = [
-            { preset: 'Hilbert3D', angle: 90, distance: 0.6, width: 0, iterations: 4, axiom: 'X', rule1: 'X=^<XF^<XFX-F^>>XFX&F+>>XFX-F>X->', rule2: '', rule3: '', rule4: '', rule5: '' },
-            { preset: 'Seaweed', angle: 22, distance: 0.3, width: 0, iterations: 4, axiom: 'G', rule1: 'G=GFGF-[&GF^GF^GF]+[^GF&GF&GF]>[^f^f&f]', rule2: '', rule3: '', rule4: '', rule5: '' },
-            { preset: 'Pythagoras tree', angle: 45, distance: 0.1, width: 0, iterations: 6, axiom: '0', rule1: '0=1F[+0F]-0F', rule2: '1=1F1F', rule3: '', rule4: '', rule5: '' },
-            { preset: 'Pythagoras tree twisted', angle: 45, distance: 0.1, width: 0, iterations: 6, axiom: '0', rule1: '0=1F[+0F]-0F', rule2: '1=1F<1F', rule3: '', rule4: '', rule5: '' },
-            { preset: 'Pythagoras tree variant', angle: 45, distance: 1, width: 0, iterations: 6, axiom: '0', rule1: '0=1F[+0F]-0F', rule2: '1=1F^1F', rule3: '', rule4: '', rule5: '' },
-            { preset: 'Fractal plant', angle: 25, distance: 0.1, width: 0, iterations: 5, axiom: 'YX', rule1: 'Y=YF<(45)YF-[-YF+YF]+[+YF-YF]', rule2: 'X=YfYf+[+YF]+[-YF]', rule3: '', rule4: '', rule5: '' },
-            { preset: 'Dragon curve', angle: 90, distance: 0.1, width: 0, iterations: 10, axiom: 'YX', rule1: 'X=XF+YF', rule2: 'Y=FX-YF', rule3: '', rule4: '', rule5: '' },
-            { preset: 'Sierpinski triangle', angle: 60, distance: 0.1, width: 0, iterations: 6, axiom: '^(90)A', rule1: 'A=BF-AF-BF', rule2: 'B=AF+BF+AF', rule3: '', rule4: '', rule5: '' },
+            { preset: 'Hilbert3D', angle: 90, distance: 0.6, width: 0.1, iterations: 4, axiom: 'X', rule1: 'X=^<XF^<XFX-F^>>XFX&F+>>XFX-F>X->', rule2: '', rule3: '', rule4: '', rule5: '' },
+            { preset: 'Seaweed', angle: 22, distance: 0.3, width: 0.1, iterations: 4, axiom: 'G', rule1: 'G=GFGF-[&GF^GF^GF]+[^GF&GF&GF]>[^f^f&f]', rule2: '', rule3: '', rule4: '', rule5: '' },
+            { preset: 'Pythagoras tree', angle: 45, distance: 0.1, width: 0.1, iterations: 6, axiom: '0', rule1: '0=1F[+0F]-0F', rule2: '1=1F1F', rule3: '', rule4: '', rule5: '' },
+            { preset: 'Pythagoras tree twisted', angle: 45, distance: 0.1, width: 0.1, iterations: 6, axiom: '0', rule1: '0=1F[+0F]-0F', rule2: '1=1F<1F', rule3: '', rule4: '', rule5: '' },
+            { preset: 'Pythagoras tree variant', angle: 45, distance: 1, width: 0.1, iterations: 6, axiom: '0', rule1: '0=1F[+0F]-0F', rule2: '1=1F^1F', rule3: '', rule4: '', rule5: '' },
+            { preset: 'Fractal plant', angle: 25, distance: 0.1, width: 0.1, iterations: 5, axiom: 'YX', rule1: 'Y=YF<(45)YF-[-YF+YF]+[+YF-YF]', rule2: 'X=YfYf+[+YF]+[-YF]', rule3: '', rule4: '', rule5: '' },
+            { preset: 'Dragon curve', angle: 90, distance: 0.1, width: 0.1, iterations: 10, axiom: 'YX', rule1: 'X=XF+YF', rule2: 'Y=FX-YF', rule3: '', rule4: '', rule5: '' },
+            { preset: 'Sierpinski triangle', angle: 60, distance: 0.1, width: 0.1, iterations: 6, axiom: '^(90)A', rule1: 'A=BF-AF-BF', rule2: 'B=AF+BF+AF', rule3: '', rule4: '', rule5: '' },
         ];
     }
 }
